ff4c2c6a10f116d17c149649d736793d6d6b9221
Evan Jaramillo <41314787+evanjaramillo@users.noreply.github.com>
2023-06-07 18:57:54 -0500

    initial commit

---
+++ src/main/java/com/mosaic/server/MosaicServer.java
@@ -0,0 +1,15 @@
+package com.mosaic.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosaicServer {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MosaicServer.class, args);
+
+	}
+
+}
