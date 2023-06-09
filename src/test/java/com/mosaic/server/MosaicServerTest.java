e75f0e45d7d100e1086dd99b6a0c4461cd83162d
Evan <41314787+evanjaramillo@users.noreply.github.com>
2023-06-08 22:58:17 -0500

    add junit

---
+++ src/test/java/com/mosaic/server/MosaicServerTest.java
@@ -0,0 +1,11 @@
+package com.mosaic.server;
+
+import com.mosaic.server.properties.TilesetProperties;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class MosaicServerTest {
+    
+
+}
