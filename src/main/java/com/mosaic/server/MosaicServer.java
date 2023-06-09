5cafe3e382100c4497d73da955664e5290518bf4
Evan <41314787+evanjaramillo@users.noreply.github.com>
2023-06-08 21:39:31 -0500

    added prettier

--- src/main/java/com/mosaic/server/MosaicServer.java
+++ src/main/java/com/mosaic/server/MosaicServer.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MosaicServer {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(MosaicServer.class, args);
-
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MosaicServer.class, args);
+    }
 }
